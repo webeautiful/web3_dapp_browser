6421593b43a00bd7f0720452a46d0cd17bcd3ddc
JamesBondMine <lujia58@outlook.com>
2023-09-05 13:09:29 +0800

    第一个版本发布

---
+++ lib/web3_dapp_browser.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library web3_dapp_browser;
+
+export 'src/web3_dapp_browser_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/web3_dapp_browser_test.dart
@@ -0,0 +1,16 @@
+import 'package:web3_dapp_browser/web3_dapp_browser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
